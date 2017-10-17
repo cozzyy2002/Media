e73df0b40eb5e50b9bb411b56ef1031c962baa2c
cozzyy2002 <cozzyy2002@gmail.com>
2017-10-17 21:55:03 +0900

    Implement IMFGetService and IPropertyStore to copy source metadate to encoded file.

--- transcode/MediaSource.cpp
+++ transcode/MediaSource.cpp
@@ -1,59 +1,159 @@
 #include "MediaSource.h"
 #include <stdio.h>
+#include <propkey.h>
+#include <propvarutil.h>
+#include <mferror.h>
+#include <typeinfo>
 
-HRESULT CMediaSource::GetCharacteristics(DWORD * pdwCharacteristics)
+static bool isSameObject(IUnknown* a, IUnknown* b) { return a == b; }
+
+/*
+	Setup inner property keys.
+	Index of each key in innerProperties is same as the index used by IPropertyStore::GetAt() method.
+*/
+HRESULT CMediaSource::setupInnerProperties(IPropertyStore* propertyStore)
 {
-	return inner->GetCharacteristics(pdwCharacteristics);
+	innerProperties.clear();
+	DWORD cProps;
+	auto hr = propertyStore->GetCount(&cProps);
+	if(SUCCEEDED(hr)) {
+		for(DWORD i = 0; i < cProps; i++) {
+			PROPERTYKEY key;
+			hr = propertyStore->GetAt(i, &key);
+			if(FAILED(hr)) break;
+			innerProperties.push_back(key);
+		}
+	}
+	return hr;
 }
 
-HRESULT CMediaSource::CreatePresentationDescriptor(IMFPresentationDescriptor ** ppPresentationDescriptor)
+// Returns whether inner object has the property.
+// Guarantees that inner IPropertyStore object is available, if returns true.
+bool CMediaSource::isInnerProperty(const PROPERTYKEY& key)
 {
-	return inner->CreatePresentationDescriptor(ppPresentationDescriptor);
+	if(innerPropertyStore) {
+		for each(auto& k in innerProperties) {
+			if(IsEqualPropertyKey(key, k)) return true;
+		}
+	}
+	return false;
 }
 
-HRESULT CMediaSource::Start(IMFPresentationDescriptor * pPresentationDescriptor, const GUID * pguidTimeFormat, const PROPVARIANT * pvarStartPosition)
+template<class I>
+static void swapObject(I** pObj, void** ppv, CMediaSource* pThis)
 {
-	return inner->Start(pPresentationDescriptor, pguidTimeFormat, pvarStartPosition);
+	printf_s(__FUNCTION__ "(%s)\n", typeid((I*)*ppv).name());
+	*pObj = (I*)*ppv;
+	*ppv = (I*)pThis;
 }
 
-HRESULT CMediaSource::Stop(void)
+CMediaSource::CMediaSource(IMFMediaSource * inner)
+	: inner(inner), innerGetService(nullptr), innerPropertyStore(nullptr)
 {
-	return inner->Stop();
+	auto hr = inner->QueryInterface(IID_PPV_ARGS(&innerGetService));
+	if(SUCCEEDED(hr)) {
+		hr = innerGetService->GetService(MF_PROPERTY_HANDLER_SERVICE, IID_PPV_ARGS(&innerPropertyStore));
+		if(SUCCEEDED(hr)) {
+			// If IPropertyStore is implemented by another object, set the object to CComPtr.
+			if(!isSameObject(inner, innerPropertyStore)) _innerPropertyStore = innerPropertyStore;
+			hr = setupInnerProperties(innerPropertyStore);
+			printf_s("Inner Property count=%d, HRESULT=0x%x.\n", innerProperties.size(), hr);
+		}
+	}
 }
 
-HRESULT CMediaSource::Pause(void)
+#pragma region Implementation of IPropertyStore
+
+HRESULT CMediaSource::GetCount(DWORD *cProps)
 {
-	return inner->Pause();
+	*cProps = innerProperties.size() + 1;
+	return S_OK;
 }
 
-HRESULT CMediaSource::Shutdown(void)
+HRESULT CMediaSource::GetAt(DWORD iProp, PROPERTYKEY *pkey)
 {
-	return inner->Shutdown();
+	auto hr(S_OK);
+	switch(iProp - innerProperties.size()) {
+	case 0:
+		*pkey = PKEY_Keywords;
+		break;
+	default:
+		if(innerPropertyStore) {
+			hr = innerPropertyStore->GetAt(iProp, pkey);
+		} else {
+			*pkey = PKEY_Null;
+			hr = E_INVALIDARG;
+		}
+		break;
+	}
+	return hr;
 }
 
-HRESULT CMediaSource::GetEvent(DWORD dwFlags, IMFMediaEvent ** ppEvent)
+HRESULT CMediaSource::GetValue(REFPROPERTYKEY key, PROPVARIANT *pv)
 {
-	return inner->GetEvent(dwFlags, ppEvent);
+	auto hr(S_OK);
+	if(isInnerProperty(key)) {
+		// If inner object has the property, return it's value.
+		hr = innerPropertyStore->GetValue(key, pv);
+	} else if(IsEqualPropertyKey(PKEY_Keywords, key)) {
+		hr = InitPropVariantFromString(L"This is my file", pv);
+	} else {
+		// Note: If the key is not present in IPropertyStore object, this method returns S_OK.
+		pv->vt = VT_EMPTY;
+	}
+	return hr;
 }
 
-HRESULT CMediaSource::BeginGetEvent(IMFAsyncCallback * pCallback, IUnknown * punkState)
+HRESULT CMediaSource::SetValue(REFPROPERTYKEY key, REFPROPVARIANT propvar)
 {
-	return inner->BeginGetEvent(pCallback, punkState);
+	return isInnerProperty(key) ? innerPropertyStore->SetValue(key, propvar) : STG_E_ACCESSDENIED;
 }
 
-HRESULT CMediaSource::EndGetEvent(IMFAsyncResult * pResult, IMFMediaEvent ** ppEvent)
+HRESULT CMediaSource::Commit(void)
 {
-	return inner->EndGetEvent(pResult, ppEvent);
+	return innerPropertyStore ? innerPropertyStore->Commit() : STG_E_ACCESSDENIED;
 }
 
-HRESULT CMediaSource::QueueEvent(MediaEventType met, REFGUID guidExtendedType, HRESULT hrStatus, const PROPVARIANT * pvValue)
+#pragma endregion
+
+#pragma region Implementation of IMFGetService
+
+HRESULT CMediaSource::GetService(REFGUID guidService, REFIID riid, LPVOID *ppvObject)
 {
-	return inner->QueueEvent(met, guidExtendedType, hrStatus, pvValue);
+	auto hr(MF_E_UNSUPPORTED_SERVICE);
+	if(IsEqualGUID(MF_PROPERTY_HANDLER_SERVICE, guidService)) {
+		// Return this pointer as IMFGetService object.
+		hr = QueryInterface(riid, ppvObject);
+	} else if(innerGetService) {
+		hr = innerGetService->GetService(guidService, riid, ppvObject);
+	}
+	return hr;
 }
+
+#pragma endregion
 
 HRESULT CMediaSource::QueryInterface(REFIID riid, void ** ppvObject)
 {
-	return inner->QueryInterface(riid, ppvObject);
+#pragma warning(push)
+#pragma warning(disable : 4838)
+	static const QITAB qitab[] = {
+		QITABENT(CMediaSource, IMFGetService),
+		QITABENT(CMediaSource, IPropertyStore),
+		{ 0 }
+	};
+#pragma warning(pop)
+
+	auto hr = QISearch(this, qitab, riid, ppvObject);
+	if(FAILED(hr)) {
+		hr = inner->QueryInterface(riid, ppvObject);
+	}
+#if TEST
+	CComHeapPtr<OLECHAR> str;
+	if(SUCCEEDED(StringFromIID(riid, &str))) {
+		wprintf_s(__FUNCTIONW__ L"(%s): HRESULT=0x%08x\n", str.m_pData, hr);
+	}
+#endif
+	return hr;
 }
 
 ULONG CMediaSource::AddRef(void)
--- transcode/MediaSource.h
+++ transcode/MediaSource.h
@@ -1,46 +1,94 @@
 #pragma once
-#include "mfidl.h"
+#include <mfidl.h>
+#include <Shlwapi.h>
+#include <vector>
+#include <atlbase.h>
 
-class CMediaSource : public IMFMediaSource
+class CMediaSource : public IMFMediaSource, public IMFGetService, public IPropertyStore
 {
 protected:
+	// Inner object and CMediaSource share reference counter, so we have inner object as raw pointer.
+	// Inner IMFMediaSource implements IMFGetService.
 	IMFMediaSource* inner;
+	IMFGetService* innerGetService;
+	IPropertyStore* innerPropertyStore;
+	CComPtr<IPropertyStore> _innerPropertyStore;
+	std::vector<PROPERTYKEY> innerProperties;
+
+	HRESULT setupInnerProperties(IPropertyStore* propertyStore);
+	bool isInnerProperty(const PROPERTYKEY& key);
 
 public:
-	CMediaSource(IMFMediaSource* inner) : inner(inner) {}
+	CMediaSource(IMFMediaSource* inner);
 	~CMediaSource() {}
+
+#pragma region Implementation of IPropertyStore
+	virtual HRESULT STDMETHODCALLTYPE GetCount(
+		/* [out] */ __RPC__out DWORD *cProps);
+	virtual HRESULT STDMETHODCALLTYPE GetAt(
+		/* [in] */ DWORD iProp,
+		/* [out] */ __RPC__out PROPERTYKEY *pkey);
+	virtual HRESULT STDMETHODCALLTYPE GetValue(
+		/* [in] */ __RPC__in REFPROPERTYKEY key,
+		/* [out] */ __RPC__out PROPVARIANT *pv);
+	virtual HRESULT STDMETHODCALLTYPE SetValue(
+		/* [in] */ __RPC__in REFPROPERTYKEY key,
+		/* [in] */ __RPC__in REFPROPVARIANT propvar);
+	virtual HRESULT STDMETHODCALLTYPE Commit(void);
+#pragma endregion
+
+#pragma region Implementation of IMFGetService
+	virtual HRESULT STDMETHODCALLTYPE GetService(
+		/* [in] */ __RPC__in REFGUID guidService,
+		/* [in] */ __RPC__in REFIID riid,
+		/* [iid_is][out] */ __RPC__deref_out_opt LPVOID *ppvObject);
+#pragma endregion
 
 #pragma region Implementation of IMFMediaSource
 	virtual HRESULT STDMETHODCALLTYPE GetCharacteristics(
-		/* [out] */ __RPC__out DWORD *pdwCharacteristics);
+		/* [out] */ __RPC__out DWORD *pdwCharacteristics) {
+		return inner->GetCharacteristics(pdwCharacteristics);
+	}
 	virtual /* [local] */ HRESULT STDMETHODCALLTYPE CreatePresentationDescriptor(
 		/* [annotation][out] */
-		_Outptr_  IMFPresentationDescriptor **ppPresentationDescriptor);
+		_Outptr_  IMFPresentationDescriptor **ppPresentationDescriptor) {
+		return inner->CreatePresentationDescriptor(ppPresentationDescriptor);
+	}
 	virtual HRESULT STDMETHODCALLTYPE Start(
 		/* [in] */ __RPC__in_opt IMFPresentationDescriptor *pPresentationDescriptor,
 		/* [unique][in] */ __RPC__in_opt const GUID *pguidTimeFormat,
-		/* [unique][in] */ __RPC__in_opt const PROPVARIANT *pvarStartPosition);
-	virtual HRESULT STDMETHODCALLTYPE Stop(void);
-	virtual HRESULT STDMETHODCALLTYPE Pause(void);
-	virtual HRESULT STDMETHODCALLTYPE Shutdown(void);
+		/* [unique][in] */ __RPC__in_opt const PROPVARIANT *pvarStartPosition) {
+		return inner->Start(pPresentationDescriptor, pguidTimeFormat, pvarStartPosition);
+	}
+	virtual HRESULT STDMETHODCALLTYPE Stop(void) { return inner->Stop(); }
+	virtual HRESULT STDMETHODCALLTYPE Pause(void) { return inner->Pause(); }
+	virtual HRESULT STDMETHODCALLTYPE Shutdown(void) { return inner->Shutdown(); }
 #pragma endregion
 
 #pragma region Implementation of IMFEventGenerator
 	virtual HRESULT STDMETHODCALLTYPE GetEvent(
 		/* [in] */ DWORD dwFlags,
-		/* [out] */ __RPC__deref_out_opt IMFMediaEvent **ppEvent);
+		/* [out] */ __RPC__deref_out_opt IMFMediaEvent **ppEvent) {
+		return inner->GetEvent(dwFlags, ppEvent);
+	}
 	virtual /* [local] */ HRESULT STDMETHODCALLTYPE BeginGetEvent(
 		/* [in] */ IMFAsyncCallback *pCallback,
-		/* [in] */ IUnknown *punkState);
+		/* [in] */ IUnknown *punkState) {
+		return inner->BeginGetEvent(pCallback, punkState);
+	}
 	virtual /* [local] */ HRESULT STDMETHODCALLTYPE EndGetEvent(
 		/* [in] */ IMFAsyncResult *pResult,
 		/* [annotation][out] */
-		_Out_  IMFMediaEvent **ppEvent);
+		_Out_  IMFMediaEvent **ppEvent) {
+		return inner->EndGetEvent(pResult, ppEvent);
+	}
 	virtual HRESULT STDMETHODCALLTYPE QueueEvent(
 		/* [in] */ MediaEventType met,
 		/* [in] */ __RPC__in REFGUID guidExtendedType,
 		/* [in] */ HRESULT hrStatus,
-		/* [unique][in] */ __RPC__in_opt const PROPVARIANT *pvValue);
+		/* [unique][in] */ __RPC__in_opt const PROPVARIANT *pvValue) {
+		return inner->QueueEvent(met, guidExtendedType, hrStatus, pvValue);
+	}
 #pragma endregion
 
 #pragma region Implementation of IUnknown
